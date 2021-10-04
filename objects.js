6846c7f8503766e3d4812d0d67963b9039cf19c6
KalyanPaladugu <kalyanchakravarthi.1994@gmail.com>
2021-10-04 07:40:28 +0530

    Arrays and Objects

---
+++ objects.js
@@ -0,0 +1,30 @@
+// Object literal
+
+// let employee={
+//     name:"Poornsai",
+//     age:20,
+//     salary:30000
+// }
+
+
+// creating instance of object 
+
+// let employee=new Object();
+// employee.name="Hindhuja";
+// employee.age=21;
+// employee.salary=28000
+
+
+// object constructor
+
+
+// function employee(name,age,salary){
+//     this.name=name;
+//     this.age=age;
+//     this.salary=salary;
+   
+// }
+
+
+// let employees=new employee("Venkatesh",23,40000)
+// document.write(employees.name+"<br>"+employees.age+"<br>"+employees.salary)
---
+++ test.js
@@ -0,0 +1,55 @@
+// var name="Venkatesh"
+
+// function demo(){
+//     var info1="password"
+// }
+
+// document.write(info1);
+// var score=3;
+
+// if(score > 3){
+//     var name="Kalyan is passed in exams"
+// }
+
+// document.write("0"+"<br>");
+// document.write("0"+"<br>");
+// document.write("0"+"<br>");
+// document.write("0")
+
+// for(let i=1;i<5;i++){
+//     document.write("0"+"<br>");
+// }
+
+// let i=1;
+// while(i<5){
+//     document.write("0"+"<br>");
+//     i++;
+// }
+
+
+// do {
+//     document.write("0"+"<br>");
+//    i++;
+// }
+// while(i<5)
+
+
+// let employee={
+//     name:"Venkatesh",
+//     salary:30000
+// }
+
+// for(data in employee){
+//     document.write(employee[data]+"<br>");
+// }
+// for(data of employee){
+//     document.write(data+"<br>");
+// }
+
+// let employees=["Venkatesh","PoornaSai","Hindhuja"]
+// for(data of employees){
+//     document.write(data+"<br>");
+// }
+// for(data in employees){
+//     document.write(data+"<br>");
+// }
